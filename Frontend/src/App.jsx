2e623f3ee7423d8159ff7ca13d65e200587af973
Michael Goldin <michaelgoldin2006@gmail.com>
2025-12-15 14:13:35 +0200

    restoring some files cuz im fucking stupid

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './assets/styles/global.css'
 import AppRouter from './router/AppRouter'
 
