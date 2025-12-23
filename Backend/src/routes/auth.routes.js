4a4907697e16628ac85940ed22b819f0b1c7a41e
Michael Goldin <michaelgoldin2006@gmail.com>
2025-12-23 13:02:06 +0200

    feat: Implement basic frontend layout and full backend user authentication.

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 // routes/auth.routes.js
 
 import express from 'express';
-import { register, login } from '../controllers/auth.controller.js'; 
+import { register, login, logout } from '../controllers/auth.controller.js'; 
 
 const router = express.Router();
 
 router.post('/register', register); 
 router.post('/login', login);
-
+router.post('/logout', logout);
 
 
 export default router;
