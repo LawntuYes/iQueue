514705e881f12c6dd24019b0fcf9fe2b9f74dbf5
tohar <toharna@gmail.com>
2025-12-23 13:59:02 +0200

    fixed navbar

--- Frontend/src/layouts/DashboardLayout.jsx
+++ Frontend/src/layouts/DashboardLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function DashboardLayout() {
   return (
     <div className="dashboard-container">
+      <Navbar />
       <aside className="sidebar">Sidebar</aside>
       <main className="dashboard-content">
         <Outlet />
