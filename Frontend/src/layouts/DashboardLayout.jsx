c73e810124f4f02cdc0915c7337e4f0cf09fcd35
Michael Goldin <michaelgoldin2006@gmail.com>
2025-12-23 14:02:06 +0200

    fixed sidebar or sum idk nig

--- Frontend/src/layouts/DashboardLayout.jsx
+++ Frontend/src/layouts/DashboardLayout.jsx
@@ -5,7 +5,6 @@ export default function DashboardLayout() {
   return (
     <div className="dashboard-container">
       <Navbar />
-      <aside className="sidebar">Sidebar</aside>
       <main className="dashboard-content">
         <Outlet />
       </main>
