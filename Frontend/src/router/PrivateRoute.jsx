05b3a0bf971066db6c2a5a52344e7444fb514846
Michael Goldin <michaelgoldin2006@gmail.com>
2026-01-26 11:03:42 +0200

    FIXED: Fixed problem with jwt token. Now all userData is saved on the token and not in LocalStorage

--- Frontend/src/router/PrivateRoute.jsx
+++ Frontend/src/router/PrivateRoute.jsx
@@ -2,7 +2,11 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function PrivateRoute() {
-  const { user } = useAuth(); // מחזיר null אם לא מחובר
+  const { user, loading } = useAuth(); 
+
+  if (loading) {
+    return <div className="loading-spinner">Loading...</div>; // Or just null/spinner component
+  }
 
   if (!user) {
     return <Navigate to="/login" replace />;
